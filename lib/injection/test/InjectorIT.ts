9397931782128fe9d09bf0fea7fa4e87cf7516ed
nilsroesel <nils.roesel@gmail.com>
2020-01-12 01:23:44 +0100

    Add test for class qualified autowired field

--- lib/injection/test/InjectorIT.ts
+++ lib/injection/test/InjectorIT.ts
@@ -6,8 +6,10 @@ describe('Test Dependency Injection', () => {
 
     it('Autowired field should be an instance', () => {
         applicationContext.load(Bar).then((bar) => {
-            expect(bar.foo).toBeTruthy('Autowired Field is unset');
+            expect(bar.foo).toBeDefined('Autowired Field is unset');
+            expect(bar.foo2).toBeDefined('Class Qualified Autowired Field is unset');
             expect(bar.foo.getMock()).toBe('works');
+            expect(bar.foo2.getMock()).toBe('works');
         });
     });
     
