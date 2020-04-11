133ef4bd9d18bc1116f5be40fe5cc4142660cc7a
nilsroesel <nils.roesel@gmail.com>
2020-04-11 22:31:28 +0200

    Define decorator for initializing a http server

---
+++ lib/server/src/decorators/application.ts
@@ -0,0 +1,41 @@
+import * as http from 'http';
+
+import { Instantiable, isInstantiable } from '../../../global-types';
+import { applicationContext } from '../../../injection/src/external';
+import { routesReadyState, ReadyStateEmitter, classFieldReadyStateEmitterComposer } from '../state';
+
+export interface ApplicationConfiguration {
+    port: number;
+    isReadyWhen: Array<ReadyStateEmitter>;
+}
+
+export function Application( configuration: Partial<ApplicationConfiguration> ) {
+    return decoratorFactory(configuration);
+}
+
+function decoratorFactory( configuration: Partial<ApplicationConfiguration> ) {
+    return <T> ( clazz: Instantiable<T> ): void | never  => {
+        if ( !isInstantiable(clazz) ) {
+            throw new Error(`Can not apply @Application for ${ clazz }.`);
+        }
+        setTimeout(() => applicationContext.load().then(() => startServer(configuration)), 1);
+    }
+}
+
+function startServer( configuration: Partial<ApplicationConfiguration> ): void {
+    const configuredReadyStates = configuration.isReadyWhen || [];
+
+    const applicationReadyState = ReadyStateEmitter.compose(
+        routesReadyState,
+        classFieldReadyStateEmitterComposer.composeRegisteredEmitters(),
+        ...configuredReadyStates
+    );
+
+    applicationReadyState.whenReady(() => {
+        http.createServer().listen(configuration.port || 80, () => {
+            console.info('INFO',`Api is up and listening on port ${ configuration.port || 80 }`);
+            console.info('INFO','Using schema http');
+        })
+    });
+
+}
