1fa6ca0a000d77a10c5b84f567ff42c7c2d40185
nilsroesel <nils.roesel@gmail.com>
2020-04-18 16:06:16 +0200

    Remove message delegation from ApiError to Error super()

--- lib/server/src/error/api.error.ts
+++ lib/server/src/error/api.error.ts
@@ -1,6 +1,6 @@
 export class ApiError extends Error implements HttpStatus {
     constructor( public readonly code: number, public readonly message: string ) {
-        super(`Error: ${ code } - ${ message }`);
+        super();
     }
 }
 
