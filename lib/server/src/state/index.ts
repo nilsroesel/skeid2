bf62ac2037f143743a16c6d6ff8b52aea743b4e9
nilsroesel <nils.roesel@gmail.com>
2020-04-18 17:47:17 +0200

    Remove deleted module from index

--- lib/server/src/state/index.ts
+++ lib/server/src/state/index.ts
@@ -1,4 +1,3 @@
-export * from './error-handler-state-emitter';
 export * from './pristine-ready-state-emitter';
 export * from './routes-ready-state-emitter';
 export * from './ready-state-emitter';
